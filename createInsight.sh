e5ec043a2b126aee616643fc8490860899eb20f7
David B <david.bergen@mx.net>
2018-07-20 00:41:46 +0000

    Update

--- createInsight.sh
+++ createInsight.sh
@@ -8,10 +8,10 @@ COIN_NAME="BitCloud"
 COIN="BTDX"
 PROJECT_POSTFIX="btdx"
 DAEMON="bitcloudd"
-DEFAULT_PORT="8555"
-RPC_PORT="8556"
-TOR_PORT="9051"
-WEB="bitcore.cc"
+DEFAULT_PORT="8329"
+RPC_PORT="8330"
+TOR_PORT="9050"
+WEB="https://bit-cloud.info"
 
 # Set targets
 NODE="1"
@@ -25,11 +25,11 @@ MESSAGE="0"
 REF_GIT="TheTrunk"
 REF_COIN="ZEL"
 REF_POSTFIX="zelcash"
-DAEMON="zelchashd"
-DEFAULT_PORT="8555"
-RPC_PORT="8556"
-TOR_PORT="9051"
-WEB="zelchash.cc"
+REF_DAEMON="zelchashd"
+REF_DEFAULT_PORT=""
+REF_RPC_PORT="16124"
+REF_TOR_PORT=""
+REF_WEB="zelchash"
 
 # Create Main Directory
 mkdir -p ${COIN_NAME}
@@ -44,12 +44,17 @@ if [[ $NODE -eq 1 ]] ; then
    # Copy repo
    PWD=$(pwd)
    DIR_NODE=${PWD}/bitcore-node-${PROJECT_POSTFIX}
-   cp bitcore-node-zelcash ${DIR_NODE}   
+   cp -r bitcore-node-zelcash ${DIR_NODE}   
 
    # Delete log files
    cd ${DIR_NODE}
    rm npm-debug.log
 
+   # Replace RPC port
+   sed -i "s/${REF_RPC_PORT}/${RPC_PORT}/g" ${DIR_NODE}/lib/services/bitcoind.js
+
+   # Replace GIT repository name
+   grep -rl "${REF_GIT}" ./ | grep -v '.git' | xargs sed -i "s/${REF_GIT}/${GIT}/g"
 
    printf "bitcore-node\n"
 fi 
